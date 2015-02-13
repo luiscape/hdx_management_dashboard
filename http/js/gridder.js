a62df2400d099a892cc40219965443dfa0708011
Luis Capelo <luiscape@gmail.com>
2015-02-12 21:58:31 -0500

    cleaning css

--- http/js/gridder.js
+++ http/js/gridder.js
@@ -1,11 +1,10 @@
-$(function(){ //DOM Ready
- 
-    $(".gridster ul").gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140],
-        min_cols: 4
-    });
-
+$(function() { //DOM Ready
+    $(".gridster > ul")
+        .gridster({
+            widget_margins: [10, 10],
+            widget_base_dimensions: [140, 140],
+            min_cols: 5
+        }).data('gridster');
 });
 
 
