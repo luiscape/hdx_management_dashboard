704a95848876ec1cd8233052e5476a2ac69afca1
Luis Capelo <luiscape@gmail.com>
2015-02-23 16:27:01 -0500

    adding first iteration of model

--- model/code/model.R
+++ model/code/model.R
@@ -3,6 +3,7 @@
 
 library(ggplot2)
 library(GGally)
+library(caret)
 
 
 onSw <- function(p = NULL, d = "tool/", a = FALSE) {
@@ -38,32 +39,164 @@ write.csv(data,"model/data/data_transformed.csv", row.names = F)
 
 ###
 # Loading data from GDocd
+readDataGdoc <- function(p) {
+  # fetching data from Google Docs
+  download.file("https://docs.google.com/spreadsheets/d/1WgalnP7XjdcfiCrSUO77i_DwWndYwRmrdV8126IC-Ts/export?format=csv",
+                p, method="wget", quiet=TRUE)
+  
+  d <- read.csv(p)
+  
+  # simple cleaning procedures
+  d <- d[,1:26]
+  d$MailChimp.Campaign <- NULL
+  d <- na.omit(d)
+  
+  # return
+  return(d)
+}
 
-data <- read.csv("model/data/data.csv")
+predictUniqueVisitors <- function(future_values = NULL, sd_multiplier = NULL) {
+  
+  # Adding future values to the right structure.
+  if (is.null(future_values) & is.null(sd_multiplier)) stop("Provide future_values or sd_multiplier.")
+  
+  # Loading and subsetting data.
+  d <- readDataGdoc("model/data/temp.csv")
+  sub <- d[
+    c("Unique.Visitors", 
+      "Number.of.Datasets", 
+      "Number.of.Registered.Users", 
+      "Datasets.Deleted", 
+      "Number.of.New.Registered.Users",
+      "Number.of.New.Datasets",
+      "Unique.Sessions",
+      "Number.of.Downloads",
+      "Number.of.Shares",
+      "Unique.Downloads",
+      "Unique.Shares",
+      "User.Register",
+      "Number.of.Organizations",
+      "Datasets.Created",
+      "Datasets.Edited",
+      "Number.of.New.Organizations",
+      "Total.Events",
+      "Number.of.Previews",
+      "Number.of.Dataset.Shares",
+      "Total.Unique.Events",
+      "Unique.Previews",
+      "Unique.Dataset.Shares"
+    )
+    ]
+  
+  # Creating model
+  # model <- lm(Unique.Visitors ~ ., data = sub)
+  model2 <- lm(Unique.Visitors ~ Unique.Shares,  data = sub)
+  
+  # Predicting values based on input.
+  if (!is.null(sd_multiplier)) {
+    # Generating output.
+    future_df <- data.frame(Unique.Shares = (sd_multiplier * sd(sub$Unique.Shares)) + mean(sub$Unique.Shares))
+    predictions <- predict(model2, future_df)
+    out <- data.frame(
+      Unique.Visitors = round(predictions,0),
+      Unique.Visitors_percent = round((predictions - mean(sub$Unique.Visitors)) / mean(sub$Unique.Visitors),2),
+      Unique.Shares = round(future_df,0),
+      Unique.Shares_percent = round((future_df$Unique.Shares - mean(sub$Unique.Shares)) / mean(sub$Unique.Shares),2)
+      )
+  }
+  else {
+    # Generating output.
+    future_df <- data.frame(Unique.Shares = future_values)
+    out <- data.frame(Unique.Visitors = round(predict(model2, future_df),0),
+                      Unique.Shares = round(future_df,0))
+  }
+  
 
+  return(out)
+}
+
+
+predictUniqueShares <- function(future_values = NULL, sd_multiplier = NULL) {
+  
+  # Adding future values to the right structure.
+  if (is.null(future_values) & is.null(sd_multiplier)) stop("Provide future_values or sd_multiplier.")
+  
+  # Loading and subsetting data.
+  d <- readDataGdoc("model/data/temp.csv")
+  sub <- d[
+    c( 
+      "Number.of.Datasets", 
+      "Number.of.Registered.Users", 
+      "Datasets.Deleted", 
+      "Number.of.New.Registered.Users",
+      "Number.of.New.Datasets",
+      "Unique.Sessions",
+      "Number.of.Downloads",
+      "Number.of.Shares",
+      "Unique.Downloads",
+      "Unique.Shares",
+      "User.Register",
+      "Number.of.Organizations",
+      "Datasets.Created",
+      "Datasets.Edited",
+      "Number.of.New.Organizations",
+      "Total.Events",
+      "Number.of.Previews",
+      "Number.of.Dataset.Shares",
+      "Total.Unique.Events",
+      "Unique.Previews",
+      "Unique.Dataset.Shares"
+    )
+    ]
+  
+  # Creating model
+  model2 <- lm(Unique.Shares ~ Datasets.Created, data = sub)
+  
+  # Predicting values based on input.
+  if (!is.null(sd_multiplier)) {
+    # Generating output.
+    future_df <- data.frame(Datasets.Created = (sd_multiplier * sd(sub$Datasets.Created)) + mean(sub$Datasets.Created))
+    predictions <- predict(model2, future_df)
+    out <- data.frame(
+      Unique.Shares = round(predictions,0),
+      Unique.Shares_percent = round((predictions - mean(sub$Unique.Shares)) / mean(sub$Unique.Shares),2),
+      Datasets.Created = round(future_df,0),
+      Datasets.Created_percent = round((future_df$Datasets.Created - mean(sub$Datasets.Created)) / mean(sub$Datasets.Created),2)
+    )
+  }
+  else {
+    # Generating output.
+    future_df <- data.frame(Unique.Shares = future_values)
+    out <- data.frame(Unique.Visitors = round(predict(model2, future_df),0),
+                      Unique.Shares = round(future_df,0))
+  }
+  
+  
+  return(out)
+}
 
 
 
 ####
 ## plotting ##
 ####
-ggplot(data) + 
-  geom_boxplot(aes(Week))
-
-
-ggpairs(data[,c(6:10,12,14:16)]) +
-  theme(text = element_text(size=12))
-
-attach(data)
-fit <- lm(Unique.Visitors ~ Number.of.Shares)
-summary(fit) ; fit
-
-fit2 <- lm(Unique.Visitors ~ Number.of.New.Users)
-summary(fit2) ; fit2
-
-fit3 <- lm(Unique.Visitors ~ Number.of.New.Datasets)
-summary(fit3) ; fit3
-
-fit4 <- lm(Unique.Visitors ~ Unique.Shares)
-summary(fit4) ; fit4
-
+# ggplot(data) + 
+#   geom_boxplot(aes(Week))
+# 
+# 
+# ggpairs(data[,c(6:10,12,14:16)]) +
+#   theme(text = element_text(size=12))
+# 
+# attach(data)
+# fit <- lm(Unique.Visitors ~ Number.of.Shares)
+# summary(fit) ; fit
+# 
+# fit2 <- lm(Unique.Visitors ~ Number.of.New.Users)
+# summary(fit2) ; fit2
+# 
+# fit3 <- lm(Unique.Visitors ~ Number.of.New.Datasets)
+# summary(fit3) ; fit3
+# 
+# fit4 <- lm(Unique.Visitors ~ Unique.Shares)
+# summary(fit4) ; fit4
+# 
